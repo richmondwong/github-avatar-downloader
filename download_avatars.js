2073e29ea7a8da08b9c8635654fc8d6b1b6954fc
Richmond Wong <richmondwong@gmail.com>
2018-12-04 01:42:59 +0000

    Added code that prints out avatar_url in the JSON/Objec

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,6 @@ var token = require('./secrets')
 
 function getRepoContributors(repoOwner, repoName, cb){
 
-
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -13,11 +12,42 @@ function getRepoContributors(repoOwner, repoName, cb){
   };
 
   request(options, function (err, res, body) {
-    cb(err, body);
+    var parsedObject = JSON.parse(body)
+    // console.log(parsedObject)
+    cb(err, parsedObject);
   })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  for (var i in result){
+    // console.log(i)
+    console.log("Result:", result[i]["avatar_url"]);
+  }
+
 });
+
+
+
+// Original Code before Step 7
+
+// function getRepoContributors(repoOwner, repoName, cb){
+
+//   var options = {
+//     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': token
+//     }
+//   };
+
+//   request(options, function (err, res, body) {
+//     cb(err, body);
+//   })
+// }
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
