280f6cb715641ed4a116c91a9ee464f4a196edea
Richmond Wong <richmondwong@gmail.com>
2018-12-04 05:37:58 +0000

    Final Code

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,21 @@
+// Import Request and FS modules
 var request = require('request');
 var fs = require('fs');
+
+//Import User Token
 var token = require('./secrets');
+
+// Take user input from the Terminal for Repo Owner and Repo Name values to pass to getRepoContributors()
 var userInputFromTerminal = process.argv.slice(2);
 var firstUserInput = userInputFromTerminal[0];
 var secondUserInput = userInputFromTerminal[1];
 
+// Throw error if user does not provide one or both of Repo Owner and Repo Name values
 if (firstUserInput === undefined || secondUserInput === undefined){
-  throw new Error("You must input Repository Owner and Repository Name!")
+  throw new Error("You must input Repository Owner and Repository Name!");
   }
 
+//Takes values for Repo Owner and Repo Name, and provides filepath for Request module to return JSON containing user details. JSON parsed into JS Object and fed to cb callback.
 function getRepoContributors(repoOwner, repoName, cb){
 
   var options = {
@@ -17,35 +24,32 @@ function getRepoContributors(repoOwner, repoName, cb){
       'User-Agent': 'request',
       'Authorization': token
     }
-  };
+  }
 
   request(options, function (err, res, body) {
-    var parsedObject = JSON.parse(body)
-    // console.log(parsedObject)
+    var parsedObject = JSON.parse(body);
     cb(err, parsedObject);
   })
 }
 
+// Download images to provided file path from provided Repo Name and Repo Owner values
 function downloadImageByURL (url, filePath) {
-request.get(url)
-.on('error', function (err) {throw err;})
-.on('response', function (response) {
-  // console.log('Response Status Code:', response.statusCode)
-})
-.pipe(fs.createWriteStream(filePath));
+  request.get(url)
+  .on('error', function (err) {throw err;})
+  .on('response', function (response) {
+    console.log('Response Status Code:', response.statusCode);
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
 
+// Prints file paths of Github user avatars of Repo Owner and Repo Name provided by user.
 getRepoContributors(firstUserInput, secondUserInput, function(err, result) {
   console.log("Errors:", err);
-
   for (var i in result){
-    // console.log(i)
-    console.log("Result:", result[i]["avatar_url"]);
-    var fileNames = result[i]["avatar_url"]
-    console.log("FileNames: ", fileNames)
-    // downloadImageByURL(result[i]["avatar_url"], filePath)
-    downloadImageByURL(fileNames, './avatars/' + [i] + ".jpg")
+    var githubUserFilePath = result[i]["avatar_url"];
+    console.log("GitHub User Avatar File Paths: ", githubUserFilePath);
+    downloadImageByURL(githubUserFilePath, './avatars/' + [i] + ".jpg");
   }
-});
+})
 
 
